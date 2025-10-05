7864222b92be16a486fad00f41137d651d1bf121
skittleguy <poopp5976@gmail.com>
2025-10-05 21:24:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const heightInput = document.getElementById("heightInput");
 const canvas = document.getElementById("canvas");
 const preview = document.getElementById("preview");
 const jsonOutput = document.getElementById("jsonOutput");
-const downloadBtn = document.getElementById("downloadBtn");
+const copyBtn = document.getElementById("copyBtn");
 
 imageInput.addEventListener("change", (event) => {
     const file = event.target.files[0];
@@ -14,48 +14,72 @@ imageInput.addEventListener("change", (event) => {
     reader.onload = function(e) {
         const img = new Image();
         img.onload = function() {
-            // Get custom size from user input
-            const sizeX = parseInt(widthInput.value) || img.width;
-            const sizeY = parseInt(heightInput.value) || img.height;
+            // Default inputs to actual image size
+            widthInput.value = img.width;
+            heightInput.value = img.height;
 
-            canvas.width = sizeX;
-            canvas.height = sizeY;
-            const ctx = canvas.getContext("2d");
-            ctx.drawImage(img, 0, 0, sizeX, sizeY);
-
-            preview.src = canvas.toDataURL();
-
-            const imageData = ctx.getImageData(0, 0, sizeX, sizeY).data;
-            const pixels = [];
-
-            // Extract pixels including alpha
-            for (let i = 0; i < imageData.length; i += 4) {
-                const r = imageData[i];
-                const g = imageData[i + 1];
-                const b = imageData[i + 2];
-                const a = imageData[i + 3];
-                pixels.push([r, g, b, a]);
-            }
-
-            const json = {
-                width: sizeX,
-                height: sizeY,
-                pixels: pixels
-            };
-
-            jsonOutput.value = JSON.stringify(json, null, 2);
+            processImage(img);
         };
         img.src = e.target.result;
     };
     reader.readAsDataURL(file);
 });
 
-downloadBtn.addEventListener("click", () => {
-    const blob = new Blob([jsonOutput.value], { type: "application/json" });
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = "pixels.json";
-    a.click();
-    URL.revokeObjectURL(url);
+function processImage(img) {
+    const sizeX = parseInt(widthInput.value) || img.width;
+    const sizeY = parseInt(heightInput.value) || img.height;
+
+    canvas.width = sizeX;
+    canvas.height = sizeY;
+    const ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0, sizeX, sizeY);
+
+    preview.src = canvas.toDataURL();
+
+    const imageData = ctx.getImageData(0, 0, sizeX, sizeY).data;
+    const pixels = [];
+
+    // Extract pixels including alpha
+    for (let i = 0; i < imageData.length; i += 4) {
+        const r = imageData[i];
+        const g = imageData[i + 1];
+        const b = imageData[i + 2];
+        const a = imageData[i + 3];
+        pixels.push([r, g, b, a]);
+    }
+
+    const json = {
+        width: sizeX,
+        height: sizeY,
+        pixels: pixels
+    };
+
+    jsonOutput.value = JSON.stringify(json, null, 2);
+}
+
+// Copy button
+copyBtn.addEventListener("click", () => {
+    jsonOutput.select();
+    jsonOutput.setSelectionRange(0, 999999); // mobile support
+    navigator.clipboard.writeText(jsonOutput.value).then(() => {
+        alert("Copied JSON to clipboard!");
+    }).catch(err => {
+        console.error("Failed to copy: ", err);
+    });
+});
+
+// If user manually changes width/height, re-process image
+widthInput.addEventListener("input", () => {
+    if (preview.src) {
+        const img = new Image();
+        img.onload = () => processImage(img);
+        img.src = preview.src;
+    }
+});
+heightInput.addEventListener("input", () => {
+    if (preview.src) {
+        const img = new Image();
+        img.onload = () => processImage(img);
+        img.src = preview.src;
+    }
 });
